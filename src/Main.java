15dc86298d6c93928790960ce777b570f05ecf29
Vadim Turanosoff <turanosoff@gmail.com>
2022-06-30 16:18:50 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int roublePerMile = 20;// рубли на милю
+        int ticketCost = 2000; // стоимость билета
+        int countMile = ticketCost / roublePerMile;
+        System.out.println("Вам начислено " + countMile + " миль"
+        );
+
+    }
+}
